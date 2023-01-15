e884dd4bac09de9efc11e90b3afb9baca722bbf5
jerry.shb <jerry.shb@alibaba-inc.com>
2023-01-15 23:41:47 +0800

    guoguo talent

---
+++ GuoguoFirstApp.java
@@ -0,0 +1,15 @@
+package com.headfirstjava.chapter1;
+
+/**
+ * Copyright (c) 2008-2019 Alibaba All Rights Reserved
+ * Created by jerry on 10:57 PM
+ *
+ * @ProjectName: HeadFirstJava
+ * @author: jerry
+ * @date 1/15/23
+ */
+public class GuoguoFirstApp {
+   public static void main(String[] args){
+       System.out.println("I'm sb");
+   }
+}
